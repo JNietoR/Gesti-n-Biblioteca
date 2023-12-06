242b1a2581f8765e8fcd00d104887a822affa682
JNietoR <jorge.nieto35@gmail.com>
2023-12-06 19:24:39 +0100

    ruta y metodo para libros en la api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\Books;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function index(){
+        $libros = Books::with('author')->get();
+        return response()->json($libros);
+    }
 }
