99d4d630343c441d924e5d41e5bbe38b1ff4dcd0
JNietoR <jorge.nieto35@gmail.com>
2023-12-06 18:08:54 +0100

    tabla libros

--- database/migrations/2023_12_06_170317_create_books_table.php
+++ database/migrations/2023_12_06_170317_create_books_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->integer('published_year');
             $table->timestamps();
         });
     }
