68bd5cede79ddbf7381e693a1a000eabd4150e25
JNietoR <jorge.nieto35@gmail.com>
2023-12-07 00:03:00 +0100

    agregar rutas al componente principal

--- resources/js/components/HelloWorld.vue
+++ resources/js/components/HelloWorld.vue
@@ -3,6 +3,15 @@
         <li>
             <router-link to="/books">Lista de Libros</router-link>
         </li>
+        <li>
+            <router-link to="/login">Login</router-link>
+        </li>
+        <li>
+            <router-link to="/registro">Registro</router-link>
+        </li>
     </ul>
     <router-view></router-view>
 </template>
+<script>
+export default {};
+</script>
