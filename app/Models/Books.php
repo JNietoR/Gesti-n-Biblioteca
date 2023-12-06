4c77371b90ece2642e0c8ccc64a491b1d9f1d197
JNietoR <jorge.nieto35@gmail.com>
2023-12-06 18:55:59 +0100

    relación 1:M entre libro y prestamos

--- app/Models/Books.php
+++ app/Models/Books.php
@@ -12,4 +12,8 @@ class Books extends Model
     public function author(){
         return $this->belongsTo(Author::class);
     }
+
+    public function loans(){
+        return $this->hasMany(Loan::class);
+    }
 }
